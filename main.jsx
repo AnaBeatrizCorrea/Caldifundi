a6492e2cf24ec1607f3603974fc84ab193b1eddd
Ana Beatriz <163312850+AnaBeatrizCorrea@users.noreply.github.com>
2026-05-16 18:26:05 -0300

    Atualização 1

---
+++ main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CaldifundiSite from './site_caldifundi_react'
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <CaldifundiSite/>
+    </React.StrictMode>
+)
