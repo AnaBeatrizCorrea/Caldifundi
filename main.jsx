c3b0660610160b92242906e332e410b7aaea4ae8
Ana Beatriz <163312850+AnaBeatrizCorrea@users.noreply.github.com>
2026-05-17 15:41:17 -0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'https://esm.sh/react'
-import ReactDOM from 'https://esm.sh/react-dom/client'
-import CaldifundiSite from './site_caldifundi_react.jsx'
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <CaldifundiSite/>
-    </React.StrictMode>
-)
